4ea73240b2dfffbd793752a8aff2f0d52d83fc52
Dmytro Balan <vt221_bdo@student.ztu.edu.ua>
2025-02-10 21:27:45 +0200

    Removed Adoption Req. Added design for Pets and Main pages

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,13 +7,13 @@
             let opacity = Math.max(1 - scrollY / 300, 0);
 
             hero.style.opacity = opacity;
-            hero.style.transform = translateY(${ scrollY / 4}px);
+            hero.style.transform = `translateY(${scrollY / 4}px)`;
 
-        if (opacity === 0) {
-            hero.style.display = "none";
-        } else {
-            hero.style.display = "flex";
-        }
-    });
+            if (opacity === 0) {
+                hero.style.pointerEvents = "none";
+            } else {
+                hero.style.pointerEvents = "auto";
+            }
+        });
     }
 });
